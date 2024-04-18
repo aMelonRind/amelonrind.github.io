365eba881807e03240e6fba9dab65a8359c79f4e
aMelonRind <kong49123@gmail.com>
2024-04-19 01:53:44 +0800

    jsm docs test

---
+++ jsm-docs-test/index.js
@@ -0,0 +1,122 @@
+function getColorMode() {
+    let colorMode = localStorage.getItem('colorMode') || "light";
+    localStorage.setItem('colorMode', colorMode);
+    return colorMode;
+}
+
+function changeColorMode() {
+    if (getColorMode() === "light") {
+        localStorage.setItem('colorMode', "dark");
+    } else {
+        localStorage.setItem('colorMode', "light");
+    }
+    document.documentElement.setAttribute("color-mode", getColorMode());
+}
+
+document.documentElement.setAttribute("color-mode", getColorMode());
+
+
+function populateClassSidebar() {
+    classLists.innerHTML = "";
+    for (const key of Array.from(classGroups).sort().reverse()) {
+        const h4 = document.createElement("h4");
+        h4.innerHTML = key;
+        classLists.appendChild(h4);
+        const div = document.createElement("div");
+        div.setAttribute("id", `${key}List`);
+        classLists.appendChild(div);
+    }
+    for (const [name, st] of searchMaps.classes) {
+        for (const clazz of st) {
+            const a = document.createElement("a");
+            a.setAttribute("href", `${officalHost}/${versionSelect.value}/${clazz.url.replace(/(#|$)/, ".html$1")}`);
+            frameLink(a);
+            a.innerHTML = name;
+            document.getElementById(`${clazz.group}List`).appendChild(a);
+        }
+    }
+}
+
+function frameLink(a) {
+    a.setAttribute("onclick", "openMain(this.href); return false;");
+}
+
+async function openMain(url, dontpush) {
+    url = url.replace(/https?:\/\/.+?\//, "/");
+    if (!dontpush) window.history.pushState({}, '', `${window.location.href.split('?')[0].replace(/#.*\??/, "")}?${url}`);
+    const req = await fetch(url);
+    if (req.status != 200) {
+        alert(`failed to load ${req.status}: \n${req.statusText}`);
+        return;
+    }
+    const text = await req.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, "text/html").getElementsByTagName("main")[0];
+    const cname = doc.getAttribute("class");
+    mainContent.setAttribute("class", cname);
+    mainContent.innerHTML = doc.innerHTML;
+    for (const a of mainContent.getElementsByTagName("a")) {
+        if (!a.hasAttribute("target") && !a.getAttribute("href")?.startsWith("#") && !a.hasAttribute("onclick")) {
+            a.setAttribute("href", a.getAttribute("href").replace(/(\.\.\/)*/, `${officalHost}/${versionSelect.value}/`));
+            frameLink(a);
+        }
+    }
+    if (cname == "searchMain") {
+        updateClassGroups();
+    }
+    scrlTo(url);
+    mainNav.parentElement.style.display = null;
+}
+
+function scrlTo(url) {
+    const scroll = url.split("#")[1];
+    if (scroll) {
+        window.scrollTo(window.scrollX, document.getElementById(scroll).offsetTop);
+        console.log(document.getElementById(scroll).offsetTop);
+        console.log(scroll);
+    } else {
+        window.scrollTo(window.scrollX, 0);
+    }
+}
+
+async function searchBox(val) {
+    if (mainContent.getAttribute("class") != "searchMain") {
+        await openMain("./search.html");
+    }
+    if (loadingSearchMap == null) {
+        console.error("loadingSearchMap is null")
+    }
+    await loadingSearchMap
+    populateClassSidebar();
+    await searchF(val);
+    for (const resultItem of document.getElementsByClassName("resultItem")) {
+        frameLink(resultItem.getElementsByTagName("a")[0]);
+    }
+}
+
+async function changeVersion() {
+    reloadSearchMap().then(populateClassSidebar);
+    openMain("general.html");
+}
+
+loadingSearchMap.then(populateClassSidebar).then(() => {
+    const rawParams = window.location.search?.substring(1) || "general.html";
+    const scroll = window.location.href.split("?")[1]?.split("#")[1];
+    openMain(scroll ? `${rawParams}#${scroll}` : rawParams);
+});
+
+menuBtn.onclick = () => {
+    if (mainNav.parentElement.style.display) {
+        mainNav.parentElement.style.display = null;
+    } else {
+        mainNav.parentElement.style.display = "block";
+    }
+};
+
+window.addEventListener("popstate", (e) => {
+    if (e.state !== null) {
+        const scroll = window.location.href.split("#")[1];
+        openMain(window.location.search.substring(1) + (scroll ? "#" + scroll : ""), true);
+    } else
+        scrlTo(window.location.href);
+});
---
+++ jsm-docs-test/search.js
@@ -0,0 +1,184 @@
+const officalHost = 'https://jsmacros.wagyourtail.xyz'
+let prevSearch = null;
+
+/**
+ * @typedef {Map<string, Set<{ name: string; url: string; group: string; }>>} SearchMap
+ * @type {{
+ *  classes: SearchMap
+ *  fields: SearchMap
+ *  methods: SearchMap
+ * }}
+ */
+const searchMaps = {
+    classes: new Map(),
+    fields: new Map(),
+    methods: new Map()
+};
+let syncId = 0n;
+let classGroups = new Set();
+let loaded = false;
+
+async function reloadSearchMap() {
+    const id = ++syncId;
+    for (const val of Object.values(searchMaps)) val.clear();
+    classGroups.clear();
+    const res = await fetch(`${officalHost}/${versionSelect.value}/search-list`);
+    if (res.status == 200) {
+        if (id !== syncId) return;
+        const text = (await res.text()).split("\n");
+        if (id !== syncId) return;
+        for (const line of text) {
+            if (line == "") continue;
+            const parts = line.split("\t");
+            switch (parts[0]) {
+                case "C":
+                    if (!searchMaps.classes.has(parts[4] ?? parts[1]))
+                        searchMaps.classes.set(parts[4] ?? parts[1], new Set());
+                    searchMaps.classes.get(parts[4] ?? parts[1]).add({
+                        name: parts[1],
+                        url: parts[2],
+                        group: parts[3] ?? "Class"
+                    });
+                    if (parts[3] && parts[3] !== "Class") classGroups.add(parts[3]);
+                    break;
+                case "M": {
+                    let methodStuff = {class: parts[1].split("#")[0], name: parts[1].split("#")[1], url: parts[2]}
+                    let cname = methodStuff.class;
+                    if (!searchMaps.classes.has(cname)) {
+                        for (const [name, st] of searchMaps.classes) {
+                            for (const clazz of st) {
+                                if (clazz.name === cname) {
+                                    cname = name;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    searchMaps.methods.set(`${cname}#${methodStuff.name}`, methodStuff);
+                    break;
+                }
+                case "F": {
+                    let fieldStuff = {class: parts[1].split("#")[0], name: parts[1].split("#")[1], url: parts[2]}
+                    let cname = fieldStuff.class;
+                    if (!searchMaps.classes.has(cname)) {
+                        for (const [name, st] of searchMaps.classes) {
+                            for (const clazz of st) {
+                                if (clazz.name === cname) {
+                                    cname = name;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    searchMaps.fields.set(`${cname}#${fieldStuff.name}`, fieldStuff);
+                    break;
+                }
+                default:
+                    alert(`unsupported line: ${line}`)
+            }
+        }
+    } else {
+        alert(`error ${res.status}\n${res.statusText}`);
+    }
+    searchMaps.classes = new Map([...searchMaps.classes.entries()].sort());
+    searchMaps.methods = new Map([...searchMaps.methods.entries()].sort());
+    searchMaps.fields = new Map([...searchMaps.fields.entries()].sort());
+    classGroups.add("Class");
+}
+
+function updateClassGroups() {
+    classGroupChecks.innerHTML = '';
+    for (const name of classGroups) {
+        const div = document.createElement("div");
+        const label = document.createElement("label");
+        label.setAttribute("for", `${name}Check`);
+        label.innerHTML = name;
+        div.appendChild(label);
+        const input = document.createElement("input");
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("id", `${name}Check`);
+        input.setAttribute("class", "SearchCheck");
+        input.setAttribute("name", `${name}Check`);
+        input.setAttribute("checked", null);
+        input.setAttribute("onclick", "searchF(search.value, true)")
+        div.appendChild(input);
+
+        classGroupChecks.appendChild(div);
+
+    }
+
+}
+
+async function searchF(val, force = false) {
+    val = val.toLowerCase();
+    await loadingSearchMap;
+    console.log(val === prevSearch);
+    if (val === prevSearch && !force) return;
+    prevSearch = val;
+    searchResults.innerHTML = "";
+    for (const group of classGroups) {
+        const groupDiv = document.createElement("div");
+        groupDiv.setAttribute("id", `${group}Results`);
+        searchResults.appendChild(groupDiv);
+    }
+    const methodDiv = document.createElement("div");
+    methodDiv.setAttribute("id", "MethodResults");
+    searchResults.appendChild(methodDiv);
+    const fieldDiv = document.createElement("div");
+    fieldDiv.setAttribute("id", "FieldResults");
+    searchResults.appendChild(fieldDiv);
+    for (const [name, st] of searchMaps.classes) {
+        for (const clazz of st) {
+            if (document.getElementById(`${clazz.group}Check`).checked) {
+                if (clazz.name.toLowerCase().includes(val)) {
+                    appendSearchResult(name, clazz.url, clazz.group);
+                }
+            }
+        }
+    }
+    if (methodsCheck.checked) {
+        for (const [name, method] of searchMaps.methods) {
+            for (const clazz of searchMaps.classes.get(`${name.split("#")[0]}`)) {
+                if (clazz.name === method.class) {
+                    if (document.getElementById(`${clazz.group}Check`).checked &&
+                        method.name.toLowerCase().includes(val)) {
+                        appendSearchResult(name, method.url, "Method");
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    if (fieldsCheck.checked) {
+        for (const [name, field] of searchMaps.fields) {
+            for (const clazz of searchMaps.classes.get(`${name.split("#")[0]}`)) {
+                if (clazz.name === field.class) {
+                    if (document.getElementById(`${clazz.group}Check`).checked &&
+                        field.name.toLowerCase().includes(val)) {
+                        appendSearchResult(name, field.url, "Field");
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function appendSearchResult(name, url, type) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "resultItem");
+    const flags = document.createElement("div");
+    flags.setAttribute("class", "flags");
+    const typed = document.createElement("div");
+    typed.setAttribute("class", `${type}Flag flag`);
+    typed.innerHTML = type[0] ?? "C";
+    flags.appendChild(typed);
+    div.appendChild(flags);
+    const a = document.createElement("a");
+    a.setAttribute("href", `${versionSelect.value}/${url.replace(/(#|$)/, ".html$1")}`)
+    a.innerHTML = name;
+    div.appendChild(a);
+    document.getElementById(`${type}Results`).appendChild(div);
+}
+
+const loadingSearchMap = reloadSearchMap();
