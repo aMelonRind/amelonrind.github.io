f6e74af607a5a40a3e2478bbb91d2b6ba6104ca9
aMelonRind <kong49123@gmail.com>
2024-12-18 10:57:02 +0800

    wasm init for mapart-conv

---
+++ mapart-conv/wasm/src/lib.rs
@@ -0,0 +1,20 @@
+
+use wasm_bindgen::prelude::*;
+
+// mod utils;
+// #[wasm_bindgen]
+// pub fn set_panic_hook() {
+//     utils::set_panic_hook();
+// }
+
+// #[wasm_bindgen]
+// extern {
+//     fn alert(s: &str);
+//     #[wasm_bindgen(js_namespace = console)]
+//     fn log(s: &str);
+// }
+
+#[wasm_bindgen]
+pub fn test(a: &[u32]) -> u64 {
+    a.iter().map(|&v| v as u64).sum()
+}
